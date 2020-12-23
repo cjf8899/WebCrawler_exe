d7d566b60b594d2deae35def0879625f9ea1a93e
cjf8899 <53032349+cjf8899@users.noreply.github.com>
2020-12-24 02:01:04 +0900

    Add files via upload

---
+++ collect_links.py
@@ -0,0 +1,325 @@
+"""
+Copyright 2018 YoongiKim
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotVisibleException, StaleElementReferenceException
+import platform
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import os.path as osp
+
+
+class CollectLinks:
+    def __init__(self, no_gui=False):
+        executable = ''
+
+        if platform.system() == 'Windows':
+            print('Detected OS : Windows')
+            executable = './chromedriver/chromedriver.exe'
+        elif platform.system() == 'Linux':
+            print('Detected OS : Linux')
+            executable = './chromedriver/chromedriver_linux'
+        elif platform.system() == 'Darwin':
+            print('Detected OS : Mac')
+            executable = './chromedriver/chromedriver_mac'
+        else:
+            raise OSError('Unknown OS Type')
+
+        if not osp.exists(executable):
+            raise FileNotFoundError('Chromedriver file should be placed at {}'.format(executable))
+
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        if no_gui:
+            chrome_options.add_argument('--headless')
+        self.browser = webdriver.Chrome(executable, chrome_options=chrome_options)
+
+        browser_version = 'Failed to detect version'
+        chromedriver_version = 'Failed to detect version'
+        major_version_different = False
+
+        if 'browserVersion' in self.browser.capabilities:
+            browser_version = str(self.browser.capabilities['browserVersion'])
+
+        if 'chrome' in self.browser.capabilities:
+            if 'chromedriverVersion' in self.browser.capabilities['chrome']:
+                chromedriver_version = str(self.browser.capabilities['chrome']['chromedriverVersion']).split(' ')[0]
+
+        if browser_version.split('.')[0] != chromedriver_version.split('.')[0]:
+            major_version_different = True
+
+        print('_________________________________')
+        print('Current web-browser version:\t{}'.format(browser_version))
+        print('Current chrome-driver version:\t{}'.format(chromedriver_version))
+        if major_version_different:
+            print('warning: Version different')
+            print('Download correct version at "http://chromedriver.chromium.org/downloads" and place in "./chromedriver"')
+        print('_________________________________')
+
+    def get_scroll(self):
+        pos = self.browser.execute_script("return window.pageYOffset;")
+        return pos
+
+    def wait_and_click(self, xpath):
+        #  Sometimes click fails unreasonably. So tries to click at all cost.
+        try:
+            w = WebDriverWait(self.browser, 15)
+            elem = w.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            elem.click()
+            self.highlight(elem)
+        except Exception as e:
+            print('Click time out - {}'.format(xpath))
+            print('Refreshing browser...')
+            self.browser.refresh()
+            time.sleep(2)
+            return self.wait_and_click(xpath)
+
+        return elem
+
+    def highlight(self, element):
+        self.browser.execute_script("arguments[0].setAttribute('style', arguments[1]);", element, "background: yellow; border: 2px solid red;")
+
+    @staticmethod
+    def remove_duplicates(_list):
+        return list(dict.fromkeys(_list))
+
+    def google(self, keyword, add_url=""):
+        self.browser.get("https://www.google.com/search?q={}&source=lnms&tbm=isch{}".format(keyword, add_url))
+
+        time.sleep(1)
+
+        print('Scrolling down')
+
+        elem = self.browser.find_element_by_tag_name("body")
+
+        for i in range(60):
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.2)
+
+        try:
+            # You may need to change this. Because google image changes rapidly.
+            # btn_more = self.browser.find_element(By.XPATH, '//input[@value="결과 더보기"]')
+            # self.wait_and_click('//input[@id="smb"]')
+            self.wait_and_click('//input[@type="button"]')
+
+            for i in range(60):
+                elem.send_keys(Keys.PAGE_DOWN)
+                time.sleep(0.2)
+
+        except ElementNotVisibleException:
+            pass
+
+        photo_grid_boxes = self.browser.find_elements(By.XPATH, '//div[@class="bRMDJf islir"]')
+
+        print('Scraping links')
+
+        links = []
+
+        for box in photo_grid_boxes:
+            try:
+                imgs = box.find_elements(By.TAG_NAME, 'img')
+
+                for img in imgs:
+                    # self.highlight(img)
+                    src = img.get_attribute("src")
+
+                    # Google seems to preload 20 images as base64
+                    if str(src).startswith('data:'):
+                        src = img.get_attribute("data-iurl")
+                    links.append(src)
+
+            except Exception as e:
+                print('[Exception occurred while collecting links from google] {}'.format(e))
+
+        links = self.remove_duplicates(links)
+
+        print('Collect links done. Site: {}, Keyword: {}, Total: {}'.format('google', keyword, len(links)))
+        self.browser.close()
+
+        return links
+
+    def naver(self, keyword, add_url=""):
+        self.browser.get("https://search.naver.com/search.naver?where=image&sm=tab_jum&query={}{}".format(keyword, add_url))
+
+        time.sleep(1)
+
+        print('Scrolling down')
+
+        elem = self.browser.find_element_by_tag_name("body")
+
+        for i in range(60):
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.2)
+
+        imgs = self.browser.find_elements(By.XPATH, '//div[@class="photo_bx api_ani_send _photoBox"]//img[@class="_image _listImage"]')
+
+        print('Scraping links')
+
+        links = []
+
+        for img in imgs:
+            try:
+                src = img.get_attribute("src")
+                if src[0] != 'd':
+                    links.append(src)
+            except Exception as e:
+                print('[Exception occurred while collecting links from naver] {}'.format(e))
+
+        links = self.remove_duplicates(links)
+
+        print('Collect links done. Site: {}, Keyword: {}, Total: {}'.format('naver', keyword, len(links)))
+        self.browser.close()
+
+        return links
+
+    def google_full(self, keyword, add_url=""):
+        print('[Full Resolution Mode]')
+
+        self.browser.get("https://www.google.com/search?q={}&tbm=isch{}".format(keyword, add_url))
+        time.sleep(1)
+
+        elem = self.browser.find_element_by_tag_name("body")
+
+        print('Scraping links')
+
+        self.wait_and_click('//div[@data-ri="0"]')
+        time.sleep(1)
+
+        links = []
+        count = 1
+
+        last_scroll = 0
+        scroll_patience = 0
+
+        while True:
+            try:
+                xpath = '//div[@id="islsp"]//div[@class="v4dQwb"]'
+                div_box = self.browser.find_element(By.XPATH, xpath)
+                self.highlight(div_box)
+
+                xpath = '//img[@class="n3VNCb"]'
+                img = div_box.find_element(By.XPATH, xpath)
+                self.highlight(img)
+
+                xpath = '//div[@class="k7O2sd"]'
+                loading_bar = div_box.find_element(By.XPATH, xpath)
+
+                # Wait for image to load. If not it will display base64 code.
+                while str(loading_bar.get_attribute('style')) != 'display: none;':
+                    time.sleep(0.1)
+
+                src = img.get_attribute('src')
+
+                if src is not None:
+                    links.append(src)
+                    print('%d: %s' % (count, src))
+                    count += 1
+
+            except StaleElementReferenceException:
+                # print('[Expected Exception - StaleElementReferenceException]')
+                pass
+            except Exception as e:
+                print('[Exception occurred while collecting links from google_full] {}'.format(e))
+
+            scroll = self.get_scroll()
+            if scroll == last_scroll:
+                scroll_patience += 1
+            else:
+                scroll_patience = 0
+                last_scroll = scroll
+
+            if scroll_patience >= 30:
+                break
+
+            elem.send_keys(Keys.RIGHT)
+
+        links = self.remove_duplicates(links)
+
+        print('Collect links done. Site: {}, Keyword: {}, Total: {}'.format('google_full', keyword, len(links)))
+        self.browser.close()
+
+        return links
+
+    def naver_full(self, keyword, add_url=""):
+        print('[Full Resolution Mode]')
+
+        self.browser.get("https://search.naver.com/search.naver?where=image&sm=tab_jum&query={}{}".format(keyword, add_url))
+        time.sleep(1)
+
+        elem = self.browser.find_element_by_tag_name("body")
+
+        print('Scraping links')
+
+        self.wait_and_click('//div[@class="photo_bx api_ani_send _photoBox"]')
+        time.sleep(1)
+
+        links = []
+        count = 1
+
+        last_scroll = 0
+        scroll_patience = 0
+
+        while True:
+            try:
+                xpath = '//div[@class="image _imageBox"]/img[@class="_image"]'
+                imgs = self.browser.find_elements(By.XPATH, xpath)
+
+                for img in imgs:
+                    self.highlight(img)
+                    src = img.get_attribute('src')
+
+                    if src not in links and src is not None:
+                        links.append(src)
+                        print('%d: %s' % (count, src))
+                        count += 1
+
+            except StaleElementReferenceException:
+                # print('[Expected Exception - StaleElementReferenceException]')
+                pass
+            except Exception as e:
+                print('[Exception occurred while collecting links from naver_full] {}'.format(e))
+
+            scroll = self.get_scroll()
+            if scroll == last_scroll:
+                scroll_patience += 1
+            else:
+                scroll_patience = 0
+                last_scroll = scroll
+
+            if scroll_patience >= 100:
+                break
+
+            elem.send_keys(Keys.RIGHT)
+            elem.send_keys(Keys.PAGE_DOWN)
+
+        links = self.remove_duplicates(links)
+
+        print('Collect links done. Site: {}, Keyword: {}, Total: {}'.format('naver_full', keyword, len(links)))
+        self.browser.close()
+
+        return links
+
+
+if __name__ == '__main__':
+    collect = CollectLinks()
+    links = collect.naver_full('박보영')
+    print(len(links), links)
---
+++ main.py
@@ -0,0 +1,410 @@
+
+import os
+import requests
+import shutil
+from multiprocessing import Pool,freeze_support
+import argparse
+from collect_links import CollectLinks
+import imghdr
+import base64
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Sites:
+    GOOGLE = 1
+    NAVER = 2
+    GOOGLE_FULL = 3
+    NAVER_FULL = 4
+
+    @staticmethod
+    def get_text(code):
+        if code == Sites.GOOGLE:
+            return 'google'
+        elif code == Sites.NAVER:
+            return 'naver'
+        elif code == Sites.GOOGLE_FULL:
+            return 'google'
+        elif code == Sites.NAVER_FULL:
+            return 'naver'
+
+    @staticmethod
+    def get_face_url(code):
+        if code == Sites.GOOGLE or Sites.GOOGLE_FULL:
+            return "&tbs=itp:face"
+        if code == Sites.NAVER or Sites.NAVER_FULL:
+            return "&face=1"
+
+
+class AutoCrawler:
+    def __init__(self, skip_already_exist=True, n_threads=4, do_google=True, do_naver=True, download_path='download',
+                 full_resolution=False, face=False, no_gui=False, limit=0):
+        """
+        :param skip_already_exist: Skips keyword already downloaded before. This is needed when re-downloading.
+        :param n_threads: Number of threads to download.
+        :param do_google: Download from google.com (boolean)
+        :param do_naver: Download from naver.com (boolean)
+        :param download_path: Download folder path
+        :param full_resolution: Download full resolution image instead of thumbnails (slow)
+        :param face: Face search mode
+        :param no_gui: No GUI mode. Acceleration for full_resolution mode.
+        :param limit: Maximum count of images to download. (0: infinite)
+        """
+
+        self.skip = skip_already_exist
+        self.n_threads = n_threads
+        self.do_google = do_google
+        self.do_naver = do_naver
+        self.download_path = download_path
+        self.full_resolution = full_resolution
+        self.face = face
+        self.no_gui = no_gui
+        self.limit = limit
+
+        os.makedirs('./{}'.format(self.download_path), exist_ok=True)
+
+    @staticmethod
+    def all_dirs(path):
+        paths = []
+        for dir in os.listdir(path):
+            if os.path.isdir(path + '/' + dir):
+                paths.append(path + '/' + dir)
+
+        return paths
+
+    @staticmethod
+    def all_files(path):
+        paths = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if os.path.isfile(path + '/' + file):
+                    paths.append(path + '/' + file)
+
+        return paths
+
+    @staticmethod
+    def get_extension_from_link(link, default='jpg'):
+        splits = str(link).split('.')
+        if len(splits) == 0:
+            return default
+        ext = splits[-1].lower()
+        if ext == 'jpg' or ext == 'jpeg':
+            return 'jpg'
+        elif ext == 'gif':
+            return 'gif'
+        elif ext == 'png':
+            return 'png'
+        else:
+            return default
+
+    @staticmethod
+    def validate_image(path):
+        ext = imghdr.what(path)
+        if ext == 'jpeg':
+            ext = 'jpg'
+        return ext  # returns None if not valid
+
+    @staticmethod
+    def make_dir(dirname):
+        current_path = os.getcwd()
+        path = os.path.join(current_path, dirname)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def get_keywords(keywords_file='keywords.txt'):
+        # read search keywords from file
+        with open(keywords_file, 'r', encoding='utf-8-sig') as f:
+            text = f.read()
+            lines = text.split('\n')
+            lines = filter(lambda x: x != '' and x is not None, lines)
+            keywords = sorted(set(lines))
+
+        print('{} keywords found: {}'.format(len(keywords), keywords))
+
+        # re-save sorted keywords
+        with open(keywords_file, 'w+', encoding='utf-8') as f:
+            for keyword in keywords:
+                f.write('{}\n'.format(keyword))
+
+        return keywords
+
+    @staticmethod
+    def save_object_to_file(object, file_path, is_base64=False):
+        try:
+            with open('{}'.format(file_path), 'wb') as file:
+                if is_base64:
+                    file.write(object)
+                else:
+                    shutil.copyfileobj(object.raw, file)
+        except Exception as e:
+            print('Save failed - {}'.format(e))
+
+    @staticmethod
+    def base64_to_object(src):
+        header, encoded = str(src).split(',', 1)
+        data = base64.decodebytes(bytes(encoded, encoding='utf-8'))
+        return data
+
+    def download_images(self, keyword, links, site_name, max_count=0):
+        self.make_dir('{}/{}'.format(self.download_path, keyword.replace('"', '')))
+        total = len(links)
+        success_count = 0
+
+        if max_count == 0:
+            max_count = total
+
+        for index, link in enumerate(links):
+            if success_count >= max_count:
+                break
+
+            try:
+                print('Downloading {} from {}: {} / {}'.format(keyword, site_name, success_count + 1, max_count))
+
+                if str(link).startswith('data:image/jpeg;base64'):
+                    response = self.base64_to_object(link)
+                    ext = 'jpg'
+                    is_base64 = True
+                elif str(link).startswith('data:image/png;base64'):
+                    response = self.base64_to_object(link)
+                    ext = 'png'
+                    is_base64 = True
+                else:
+                    response = requests.get(link, stream=True)
+                    ext = self.get_extension_from_link(link)
+                    is_base64 = False
+
+                no_ext_path = '{}/{}/{}_{}'.format(self.download_path.replace('"', ''), keyword, site_name, str(index).zfill(4))
+                path = no_ext_path + '.' + ext
+                self.save_object_to_file(response, path, is_base64=is_base64)
+
+                success_count += 1
+                del response
+
+                ext2 = self.validate_image(path)
+                if ext2 is None:
+                    print('Unreadable file - {}'.format(link))
+                    os.remove(path)
+                    success_count -= 1
+                else:
+                    if ext != ext2:
+                        path2 = no_ext_path + '.' + ext2
+                        os.rename(path, path2)
+                        print('Renamed extension {} -> {}'.format(ext, ext2))
+
+            except Exception as e:
+                print('Download failed - ', e)
+                continue
+
+    def download_from_site(self, keyword, site_code):
+        site_name = Sites.get_text(site_code)
+        add_url = Sites.get_face_url(site_code) if self.face else ""
+
+        try:
+            collect = CollectLinks(no_gui=self.no_gui)  # initialize chrome driver
+        except Exception as e:
+            print('Error occurred while initializing chromedriver - {}'.format(e))
+            return
+
+        try:
+            print('Collecting links... {} from {}'.format(keyword, site_name))
+
+            if site_code == Sites.GOOGLE:
+                links = collect.google(keyword, add_url)
+
+            elif site_code == Sites.NAVER:
+                links = collect.naver(keyword, add_url)
+
+            elif site_code == Sites.GOOGLE_FULL:
+                links = collect.google_full(keyword, add_url)
+
+            elif site_code == Sites.NAVER_FULL:
+                links = collect.naver_full(keyword, add_url)
+
+            else:
+                print('Invalid Site Code')
+                links = []
+
+            print('Downloading images from collected links... {} from {}'.format(keyword, site_name))
+            self.download_images(keyword, links, site_name, max_count=self.limit)
+
+            print('Done {} : {}'.format(site_name, keyword))
+
+        except Exception as e:
+            print('Exception {}:{} - {}'.format(site_name, keyword, e))
+
+    def download(self, args):
+        self.download_from_site(keyword=args[0], site_code=args[1])
+
+    def do_crawling(self):
+        keywords = self.get_keywords()
+
+        tasks = []
+
+        for keyword in keywords:
+            dir_name = '{}/{}'.format(self.download_path, keyword)
+            if os.path.exists(os.path.join(os.getcwd(), dir_name)) and self.skip:
+                print('Skipping already existing directory {}'.format(dir_name))
+                continue
+
+            if self.do_google:
+                if self.full_resolution:
+                    tasks.append([keyword, Sites.GOOGLE_FULL])
+                else:
+                    tasks.append([keyword, Sites.GOOGLE])
+
+            if self.do_naver:
+                if self.full_resolution:
+                    tasks.append([keyword, Sites.NAVER_FULL])
+                else:
+                    tasks.append([keyword, Sites.NAVER])
+
+        pool = Pool(self.n_threads)
+        pool.map_async(self.download, tasks)
+        pool.close()
+        pool.join()
+        print('Task ended. Pool join.')
+
+        self.imbalance_check()
+
+        print('End Program')
+
+    def imbalance_check(self):
+        print('Data imbalance checking...')
+
+        dict_num_files = {}
+
+        for dir in self.all_dirs(self.download_path):
+            n_files = len(self.all_files(dir))
+            dict_num_files[dir] = n_files
+
+        avg = 0
+        for dir, n_files in dict_num_files.items():
+            avg += n_files / len(dict_num_files)
+            print('dir: {}, file_count: {}'.format(dir, n_files))
+
+        dict_too_small = {}
+
+        for dir, n_files in dict_num_files.items():
+            if n_files < avg * 0.5:
+                dict_too_small[dir] = n_files
+
+        if len(dict_too_small) >= 1:
+            print('Data imbalance detected.')
+            print('Below keywords have smaller than 50% of average file count.')
+            print('I recommend you to remove these directories and re-download for that keyword.')
+            print('_________________________________')
+            print('Too small file count directories:')
+            for dir, n_files in dict_too_small.items():
+                print('dir: {}, file_count: {}'.format(dir, n_files))
+
+            print("Remove directories above? (y/n)")
+            answer = input()
+
+            if answer == 'y':
+                # removing directories too small files
+                print("Removing too small file count directories...")
+                for dir, n_files in dict_too_small.items():
+                    shutil.rmtree(dir)
+                    print('Removed {}'.format(dir))
+
+                print('Now re-run this program to re-download removed files. (with skip_already_exist=True)')
+        else:
+            print('Data imbalance not detected.')
+
+def crawling(limit_num):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--skip', type=str, default='true',
+                        help='Skips keyword already downloaded before. This is needed when re-downloading.')
+    parser.add_argument('--threads', type=int, default=1, help='Number of threads to download.')
+    parser.add_argument('--google', type=str, default='true', help='Download from google.com (boolean)')
+    parser.add_argument('--naver', type=str, default='true', help='Download from naver.com (boolean)')
+    parser.add_argument('--full', type=str, default='false', help='Download full resolution image instead of thumbnails (slow)')
+    parser.add_argument('--face', type=str, default='true', help='Face search mode')
+    parser.add_argument('--no_gui', type=str, default='auto', help='No GUI mode. Acceleration for full_resolution mode. '
+                                                                   'But unstable on thumbnail mode. '
+                                                                    'Default: "auto" - false if full=false, true if full=true')
+    args = parser.parse_args()
+
+    _skip = False if str(args.skip).lower() == 'false' else True
+    _threads = args.threads
+    _google = False if str(args.google).lower() == 'false' else True
+    _naver = False if str(args.naver).lower() == 'false' else True
+    _full = False if str(args.full).lower() == 'false' else True
+    _face = False if str(args.face).lower() == 'false' else True
+    
+    _limit = int(limit_num)
+
+    no_gui_input = str(args.no_gui).lower()
+    if no_gui_input == 'auto':
+        _no_gui = _full
+    elif no_gui_input == 'true':
+        _no_gui = True
+    else:
+        _no_gui = False
+
+    print('Options - skip:{}, threads:{}, google:{}, naver:{}, full_resolution:{}, face:{}, no_gui:{}, limit:{}'
+          .format(_skip, _threads, _google, _naver, _full, _face, _no_gui, _limit))
+
+    crawler = AutoCrawler(skip_already_exist=_skip, n_threads=_threads,
+                          do_google=_google, do_naver=_naver, full_resolution=_full,
+                          face=_face, no_gui=_no_gui, limit=_limit)
+    crawler.do_crawling()
+
+class PushButtonWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(300,100)
+        self.setupUI()
+
+
+    def setupUI(self):
+        self.setWindowTitle("Crawler")
+
+        btn = QPushButton("크롤링", self)
+        btn.move(185, 50)
+        btn.clicked.connect(self.btn_clicked)
+
+        self.label = QLabel("검색어", self)
+        self.label.move(5,20)
+        self.label.resize(100,20)
+
+        self.lineEdit = QLineEdit("", self)
+        self.lineEdit.move(50,20)
+        self.lineEdit.resize(100,20)
+        self.lineEdit.textChanged.connect(self.lineEdit_textChanged)
+
+        self.label = QLabel("개수", self)
+        self.label.move(155,20)
+        self.label.resize(100,20)
+
+        self.lineEdit2 = QLineEdit("", self)
+        self.lineEdit2.move(185,20)
+        self.lineEdit2.resize(100,20)
+        self.lineEdit2.textChanged.connect(self.lineEdit_textChanged2)
+
+
+    def btn_clicked(self):
+        QMessageBox.about(self, "Start Crawling", "'" + self.lineEdit.text()+"' 크롤링을 시작합니다.")
+        f = open("./keywords.txt", 'w',encoding='utf8')
+        f.write(self.lineEdit.text())
+        f.close()
+        print(self.lineEdit.text())
+        crawling(self.lineEdit2.text())
+        exit()
+
+    def  lineEdit_textChanged(self):
+        pass
+        #self.statusBar.showMessage(self.lineEdit.text())
+
+    def  lineEdit_textChanged2(self):
+        pass
+
+
+if __name__ == '__main__':
+    freeze_support()
+    app = QApplication(sys.argv)
+    window = PushButtonWindow()
+    window.show()
+    app.exec_()
+
